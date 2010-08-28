a9953a0140ca7a01a9b7ac1cc592023e951e5e01
Toshiyuki Takahashi <toshi@think.ttoshi.ddo.jp>
2010-08-28 21:21:49 +0900

    change to load skk,sticky-shift,w3m

--- my-emacs-lisp-init.el
+++ my-emacs-lisp-init.el
@@ -1,5 +1,7 @@
+(load "skk-sticky-shift-config.el")
 (load "alc.el")
 (load "window-config.el")
 (load "dired-config.el")
 (load "exchange-lines.el")
 (load "tvvitter-config.el")
+(load "w3m-config.el")
