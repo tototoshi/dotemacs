531e987f8c03d31bd59e4bb990c2b229ccf98494
Toshiyuki Takahashi <toshi@think.ttoshi.ddo.jp>
2010-08-28 16:16:23 +0900

    add window-config.el

---
+++ window-config.el
@@ -0,0 +1,15 @@
+;;http://d.hatena.ne.jp/rubikitch/20100210/emacs
+(defun other-window-or-split ()
+  (interactive)
+  (when (one-window-p)
+    (split-window-horizontally))
+  (other-window 1))
+
+(global-set-key (kbd "C-,") 'other-window-or-split)
+
+
+;; enlarge & shrink
+(global-set-key [C-S-up] 'enlarge-window)
+(global-set-key [C-S-down] 'shrink-window)
+(global-set-key [C-S-right] 'enlarge-window-horizontally)
+(global-set-key [C-S-left] 'shrink-window-horizontally)
