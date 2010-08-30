0bebaadaa465fee0c01c0a607b04622b2857205c
toshiyuki takahashi <toshiyuki.takahashi@hde.co.jp>
2010-08-30 13:09:32 +0900

    add Pymacs configuration

--- python-config.el
+++ python-config.el
@@ -5,3 +5,15 @@
 ;; ln[1]  ->  >>>
 (setq py-python-command-args '("-cl"))
 
+;; Pymacs
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+
+(let ((pymacs-lisp-dir "~/.emacs.d/pymacs-lisp"))
+  (if (not (file-exists-p pymacs-lisp-dir))
+      (make-directory pymacs-lisp-dir))
+  (eval-after-load "pymacs"
+    '(add-to-list 'pymacs-load-path pymacs-lisp-dir)))
