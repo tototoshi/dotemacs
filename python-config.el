a7dd787d1897860256590ab373e33b0dd23cfb5c
toshiyuki takahashi <toshiyuki.takahashi@hde.co.jp>
2010-08-30 19:24:49 +0900

    add         add-mode-alist         interpreter-mode-alist         python-mode-hook

--- python-config.el
+++ python-config.el
@@ -12,6 +12,15 @@
 (autoload 'pymacs-exec "pymacs" nil t)
 (autoload 'pymacs-load "pymacs" nil t)
 
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+
+(add-hook 'python-mode-hook
+           (lambda ()
+             (set (make-variable-buffer-local 'beginning-of-defun-function)
+                  'py-beginning-of-def-or-class)
+             (setq outline-regexp "def\\|class ")))
+
 (let ((pymacs-lisp-dir "~/.emacs.d/pymacs-lisp"))
   (if (not (file-exists-p pymacs-lisp-dir))
       (make-directory pymacs-lisp-dir))
