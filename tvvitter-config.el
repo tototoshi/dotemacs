2fa6021f0f7f3e7f01c70af4f16b5847991884ef
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2010-09-07 21:12:59 +0900

    modify function 'switch-to-tvvitter' Start tvvitter-mode automatically when buffer named "*tvvitter*" not exist.

--- tvvitter-config.el
+++ tvvitter-config.el
@@ -1,10 +1,18 @@
 (require 'tvvitter)
 ;; リスト機能を使うための設定
-(setq tvvitter-list-candidates-alist 
-      '(("tototoshi" . "abbey")))
+(setq tvvitter-list-candidates-alist
+      '(("tototoshi" . "abbey")
+	("tototoshi" . "hde")))
 
 ;; こうやると *tvvitter* バッファへCtrl+c tでジャンプできる。
 (defun switch-to-tvvitter ()
   (interactive)
-  (switch-to-buffer "*tvvitter*"))
+  (let ((tvvitter-buffer-name "*tvvitter*"))
+    (if (get-buffer tvvitter-buffer-name)
+	(switch-to-buffer tvvitter-buffer-name)
+      (tvvitter-mode))))
+
 (global-set-key "\C-ct" 'switch-to-tvvitter)
+
+
+
