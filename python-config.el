42236f8c5037a04ef44afce4396f58b2c39a697f
toshiyuki takahashi <toshiyuki.takahashi@hde.co.jp>
2010-09-08 10:58:51 +0900

    not use ipython

--- python-config.el
+++ python-config.el
@@ -1,9 +1,9 @@
 (require 'python-mode)
-(require 'ipython)
 
+;; (require 'ipython)
 ;; ipythonのプロンプトを替える
 ;; ln[1]  ->  >>>
-(setq py-python-command-args '("-cl"))
+;; (setq py-python-command-args '("-cl"))
 
 ;; Pymacs
 (autoload 'pymacs-apply "pymacs")
