e8f9e5cba667489133c1bc57670049522431b9b0
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2010-11-08 22:12:40 +0900

    use macro 'my-make-view-mode-out-command'

--- my-view-mode-config.el
+++ my-view-mode-config.el
@@ -4,24 +4,28 @@
 (require 'view)
 (global-set-key (kbd "C-j") 'view-mode)
 
+(defvar my-view-mode-prefix "my-view-mode-")
+
 (defmacro in-view-mode (&rest forms)
   `(progn (view-mode)
           ,@forms
           (view-mode)))
 
 (defmacro my-make-view-mode-command (key key-name fn)
-  (let* ((prefix "my-view-mode-")
-        (view-mode-fn-name (concat prefix key-name)))
+  (let* ((view-mode-fn-name (concat my-view-mode-prefix key-name)))
     `(progn (defun ,(intern view-mode-fn-name) ()
               (interactive)
               (in-view-mode
                (funcall ,fn)))
             (define-key view-mode-map ,key (quote ,(intern view-mode-fn-name))))))
 
-(defun my-view-mode-yank ()
-  (interactive)
-  (view-mode)
-  (yank))
+(defmacro my-make-view-mode-out-command (key key-name fn)
+  (let* ((view-mode-fn-name (concat my-view-mode-prefix key-name)))
+    `(progn (defun ,(intern view-mode-fn-name) ()
+              (interactive)
+              (view-mode)
+	      (funcall ,fn))
+            (define-key view-mode-map ,key (quote ,(intern view-mode-fn-name))))))
 
 (defun my-view-mode-r (new-char)
   (interactive "sChar: ")
@@ -29,22 +33,12 @@
    (delete-char 1)
    (insert new-char)))
 
-(defun my-view-mode-a ()
-  (interactive)
-  (view-mode)
-  (forward-char))
-
-(defun my-view-mode-A ()
-  (interactive)
-  (view-mode)
-  (move-end-of-line 1))
-
-(defun my-view-mode-C-d ()
-  (interactive)
-  (view-mode)
-  (delete-char 1))
-
+(my-make-view-mode-out-command "p" "p" #'yank)
+(my-make-view-mode-out-command "a" "a" #'forward-char)
+(my-make-view-mode-out-command "A" "A" #'(lambda () (move-end-of-line 1)))
+(my-make-view-mode-out-command "\C-d" "delete-char" #'(lambda () (delete-char 1)))
 (my-make-view-mode-command "x" "x" #'(lambda () (delete-char 1)))
+(my-make-view-mode-command "X" "X" #'(lambda () (delete-backward-char 1)))
 (my-make-view-mode-command " " "space" #'(lambda () nil))
 (my-make-view-mode-command "u" "u" #'undo)
 (my-make-view-mode-command "0" "0" #'(lambda () (move-beginning-of-line 1)))
@@ -54,10 +48,7 @@
 (add-hook 'view-mode-hook '(lambda ()
                              (setq view-read-only t)
                              (define-key view-mode-map "\C-j" 'view-mode)
-                             (define-key view-mode-map "\C-d" 'my-view-mode-C-d)
                              (define-key view-mode-map "i" 'view-mode)
-                             (define-key view-mode-map "a" 'my-view-mode-a)
-                             (define-key view-mode-map "A" 'my-view-mode-A)
                              (define-key view-mode-map "b" 'backward-word)
                              (define-key view-mode-map "w" 'forward-word)
                              (define-key view-mode-map "h" 'backward-char)
@@ -66,6 +57,5 @@
                              (define-key view-mode-map "k" 'previous-line)
                              (define-key view-mode-map "K" 'backward-paragraph)
                              (define-key view-mode-map "l" 'forward-char)
-                             (define-key view-mode-map "p" 'my-view-mode-yank)
                              (define-key view-mode-map "r" 'my-view-mode-r)))
 
