35b652e02ecfbbbf9b7f341426e0f5e0a35c9fe4
toshiyuki takahashi <toshiyuki.takahashi@hde.co.jp>
2010-12-01 19:25:34 +0900

    remove unused functions

--- dired-config.el
+++ dired-config.el
@@ -12,7 +12,7 @@
              (define-key dired-mode-map "n" 'dired-advertised-find-file)
              (define-key dired-mode-map "j" 'dired-next-line)
              (define-key dired-mode-map "k" 'dired-previous-line)
-	     (define-key dired-mode-map "o" 'dired-open-file)
+             (define-key dired-mode-map "o" 'dired-open-file)
              (define-key dired-mode-map "v" 'dired-open-with-evince)))
 
 (defun dired-open-eshell ()
@@ -22,14 +22,6 @@
     (eshell-kill-input)
     (cd current-dir)
     (eshell-send-input)))
-
-(defun dired-open-current-directory ()
-  (interactive)
-  (dired "."))
-
-(defun dired-open-with-evince ()
-  (interactive)
-  (dired-do-async-shell-command "evince" t (dired-get-marked-files t nil) ))
 
 (defun dired-open-file ()
   "In dired, open the file named on this line."
