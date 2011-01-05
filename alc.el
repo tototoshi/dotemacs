45e0aafb2efc620f21940a5dc30e43c1914e62aa
toshiyuki takahashi <toshiyuki.takahashi@hde.co.jp>
2011-01-05 15:18:47 +0900

    refactoring

--- alc.el
+++ alc.el
@@ -1,11 +1,24 @@
 (require 'http-get)
 
-(defvar alc-toppage-url "http://eow.alc.co.jp/")
+(defvar alc-toppage-url "http://eow.alc.co.jp")
+(defvar alc-encoding 'utf-8)
+(defvar alc-encoding-str "UTF-8")
+(defvar alc-http-param-alist '(("ref" . "sa")))
 
-(defun alc (arg)
-  (interactive "sWORD: ")
-  (cond ((string= arg "") (alc-get-default-word))
-        (t (browse-url (alc-make-url arg)))))
+(defun alc-http-param ()
+  (reduce #'(lambda (x y) (concat x "&" y))
+          (mapcar #'(lambda (cons)
+                      (format "%s=%s"
+                              (first cons)
+                              (http-url-encode (rest cons) alc-encoding)))
+                  alc-http-param-alist)))
+
+(defun alc-make-url (word)
+  (format "%s/%s/%s/?%s"
+          alc-toppage-url
+          (http-url-encode word alc-encoding)
+          alc-encoding-str
+          (alc-http-param)))
 
 (defun alc-get-default-word ()
   (cond ((bounds-of-thing-at-point 'word)
@@ -14,8 +27,11 @@
            (buffer-substring-no-properties start end)))
         (t "")))
 
-(defun alc-make-url (word)
-  (concat alc-toppage-url  (http-url-encode word 'utf-8) "/UTF-8/?ref=sa"))
+(defun alc (arg)
+  (interactive "sWORD: ")
+  (browse-url (alc-make-url
+               (cond ((string= arg "") (alc-get-default-word))
+                     (t arg)))))
 
-(defun alc-url-retrieve (word)
-  (url-retrieve-synchronously (alc-make-url word)))
+(global-set-key "\C-c\C-w" 'alc)
+
