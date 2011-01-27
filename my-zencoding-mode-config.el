56fbfd154270a47f67f167296b49cb78bc3cd80c
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2011-01-28 00:37:44 +0900

    zencoding-mode

---
+++ my-zencoding-mode-config.el
@@ -0,0 +1,7 @@
+;;;;;;;;;;;;;;;;;;;;
+;; zencoding-mode ;;
+;;;;;;;;;;;;;;;;;;;;
+(require 'zencoding-mode)
+(add-hook 'sgml-mode-hook 'zencoding-mode)
+(define-key zencoding-mode-keymap (kbd "C-c C-m") 'zencoding-expand-line)
+(define-key zencoding-preview-keymap (kbd "C-c C-m") 'zencoding-preview-accept)
