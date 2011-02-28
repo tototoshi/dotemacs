b913a141bb032664834825f11880c59954739825
toshi <toshi@toshi-ThinkPad-X201.(none)>
2011-02-28 21:14:58 +0900

    shell-command-to-string -> shell-command

--- my-rfc.el
+++ my-rfc.el
@@ -8,6 +8,6 @@
     (if (not (file-exists-p dir))
         (mkdir dir))
     (cond ((file-exists-p file) nil)
-          (t (shell-command-to-string
+          (t (shell-command
               (format "wget -O %s/rfc%d.txt http://tools.ietf.org/rfc/rfc%d.txt" dir n n))))
     (find-file-read-only file)))
