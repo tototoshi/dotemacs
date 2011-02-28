7d1d1baaf8043dbba6fc8ab8c7836ce4f7d62784
Toshiyuki Takahashi <toshi@ubuntu-vb.(none)>
2011-02-28 13:20:05 +0900

    mkdir

--- my-rfc.el
+++ my-rfc.el
@@ -1,8 +1,12 @@
 ;; rfc
+(defvar my-rfc-directory "~/Documents/rfc")
+
 (defun rfc (n)
   (interactive "nNo.: ")
-  (let* ((dir "~/rfc")
+  (let* ((dir my-rfc-directory)
          (file (format "%s/rfc%d.txt" dir n)))
+    (if (not (file-exists-p dir))
+        (mkdir dir))
     (cond ((file-exists-p file) nil)
           (t (shell-command-to-string
               (format "wget -O %s/rfc%d.txt http://tools.ietf.org/rfc/rfc%d.txt" dir n n))))
