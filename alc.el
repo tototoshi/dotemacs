63378b8c86478653c941dfc6f22b19cd38ace2ca
toshi <toshi@toshi-ThinkPad-X201.(none)>
2011-03-01 21:49:04 +0900

    fix a bug when parameter is blank. use (current-word) instead of (alc-get-default-word).

--- alc.el
+++ alc.el
@@ -14,23 +14,17 @@
                   alc-http-param-alist)))
 
 (defun alc-make-url (word)
-  (format "%s/%s/%s/?%s"
-          alc-toppage-url
-          (http-url-encode word alc-encoding)
-          alc-encoding-str
-          (alc-http-param)))
-
-(defun alc-get-default-word ()
-  (cond ((bounds-of-thing-at-point 'word)
-         (let ((start (first   (bounds-of-thing-at-point 'word)))
-               (end   (rest    (bounds-of-thing-at-point 'word))))
-           (buffer-substring-no-properties start end)))
-        (t "")))
+  (cond ((string= word "") alc-toppage-url)
+        (t (format "%s/%s/%s/?%s"
+                   alc-toppage-url
+                   (http-url-encode word alc-encoding)
+                   alc-encoding-str
+                   (alc-http-param)))))
 
 (defun alc (arg)
   (interactive "sWORD: ")
   (browse-url (alc-make-url
-               (cond ((string= arg "") (alc-get-default-word))
+               (cond ((string= arg "") (or (current-word) ""))
                      (t arg)))))
 
 (global-set-key "\C-c\C-w" 'alc)
