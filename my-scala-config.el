7119e240d84c2f1fc4ff4c88708bfe1aa9018065
toshi <toshi@toshi-vb.(none)>
2011-05-11 17:29:31 +0900

    auto-mode-alist

--- my-scala-config.el
+++ my-scala-config.el
@@ -1,3 +1,6 @@
+(require 'scala-mode)
+(add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
+
 ;; http://d.hatena.ne.jp/tototoshi/20110122/1295689870
 (defadvice scala-eval-region (after pop-after-scala-eval-region activate)
   (pop-to-buffer scala-inf-buffer-name)
