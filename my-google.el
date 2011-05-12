a6d284e9a0c9631abd9d3607004ad43d463a67f6
toshi <toshi@toshi-vb.(none)>
2011-05-12 11:31:35 +0900

    resolve dependency

--- my-google.el
+++ my-google.el
@@ -1,3 +1,7 @@
+(require 'http-get)
+
 (defun google (word)
   (interactive "sSearch: ")
   (browse-url (format "http://www.google.co.jp/search?q=%s" (http-url-encode word 'utf-8))))
+
+(provide 'google)
