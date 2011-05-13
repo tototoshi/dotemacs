ec0413c4c536fdecf0157e6f7d90cd97a27e0d0b
toshiyuki <toshiyuki.takahashi@hde.co.jp>
2011-05-13 21:47:11 +0900

    slime configuration

--- my-slime-config.el
+++ my-slime-config.el
@@ -1,4 +1,28 @@
+(require 'anything-kyr-config)
+(require 'anything-complete)
 (require 'slime)
-(slime-setup '(slime-repl))
+
 (setq slime-net-coding-system 'utf-8-unix)
+
+(eval-after-load "slime"
+  '(progn
+     (message "Slime configuration...")
+     (slime-setup '(slime-fancy
+                    slime-repl
+                    slime-asdf
+                    anything-slime))))
+
+(add-hook
+ 'anything-slime-init-hook
+ (lambda ()
+   (add-to-list 'anything-kyr-commands-by-condition
+                '(slime-net-processes
+                  anything-slime-list-connections))
+   (define-key slime-mode-map (kbd "TAB") 'anything-slime-complete)
+   (define-key slime-repl-mode-map (kbd "TAB") 'anything-slime-complete)
+   (define-key slime-repl-mode-map "\M-r" 'anything-slime-repl-history)
+   ))
+
+
+
 
