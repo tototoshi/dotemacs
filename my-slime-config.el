7a08d44f5cb90e0a2cc5315df0018a66e3dab16d
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2011-05-16 10:00:03 +0900

    use ac-clime instead of anything-slime

--- my-slime-config.el
+++ my-slime-config.el
@@ -12,17 +12,20 @@
                     slime-asdf
                     anything-slime))))
 
-(add-hook
- 'anything-slime-init-hook
- (lambda ()
-   (add-to-list 'anything-kyr-commands-by-condition
-                '(slime-net-processes
-                  anything-slime-list-connections))
-   (define-key slime-mode-map (kbd "TAB") 'anything-slime-complete)
-   (define-key slime-repl-mode-map (kbd "TAB") 'anything-slime-complete)
-   (define-key slime-repl-mode-map "\M-r" 'anything-slime-repl-history)
-   ))
+;; (add-hook
+;;  'anything-slime-init-hook
+;;  (lambda ()
+;;    (add-to-list 'anything-kyr-commands-by-condition
+;;                 '(slime-net-processes
+;;                   anything-slime-list-connections))
+;;    (define-key slime-mode-map (kbd "M-/") 'anything-slime-complete)
+;;    (define-key slime-repl-mode-map (kbd "TAB") 'anything-slime-complete)
+;;    (define-key slime-repl-mode-map "\M-r" 'anything-slime-repl-history)
+;;    ))
 
 
+(require 'ac-slime)
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 
 
