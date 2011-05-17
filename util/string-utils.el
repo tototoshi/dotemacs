3dd8344a48759b9fab1c36e03962b65271f70195
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2011-05-17 23:11:42 +0900

    string-left-trim string-right-trim string-trim

--- util/string-utils.el
+++ util/string-utils.el
@@ -1,3 +1,21 @@
+B(defun string-left-trim (character-bag string)
+  (let ((c-list (string-to-char-list string))
+        (bag-list (string-to-char-list character-bag)))
+    (apply #'concat
+           (mapcar #'char-to-string
+                   (drop-while #'(lambda (c) (find c bag-list)) c-list)))))
+
+(defun string-right-trim (character-bag string)
+  (let ((c-list (reverse (string-to-char-list string)))
+        (bag-list (string-to-char-list character-bag)))
+    (apply #'concat
+           (mapcar #'char-to-string
+                   (reverse (drop-while #'(lambda (c) (find c bag-list)) c-list))))))
+
+(defun string-trim (character-bag string)
+  (string-left-trim character-bag
+                    (string-right-trim character-bag string)))
+
 (defun chomp (str)
      "..."
      (let ((s (if (symbolp str)(symbol-name str) str)))
