9399d58d731a12324384e57b1545373e10855fa6
toshiyuki <toshiyuki.takahashi@hde.co.jp>
2011-05-18 17:38:12 +0900

    ちょっとへんこう

--- my-scala-config.el
+++ my-scala-config.el
@@ -1,11 +1,11 @@
-(require 'scala-mode)
-(add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
+(when (require 'scala-mode nil t)
+  (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
 
-;; http://d.hatena.ne.jp/tototoshi/20110122/1295689870
-(defadvice scala-eval-region (after pop-after-scala-eval-region activate)
-  (pop-to-buffer scala-inf-buffer-name)
-  (goto-char (point-max)))
+  ;; http://d.hatena.ne.jp/tototoshi/20110122/1295689870
+  (defadvice scala-eval-region (after pop-after-scala-eval-region activate)
+    (pop-to-buffer scala-inf-buffer-name)
+    (goto-char (point-max)))
 
-(defadvice scala-eval-buffer (after pop-after-scala-eval-buffer activate)
-  (pop-to-buffer scala-inf-buffer-name)
-  (goto-char (point-max)))
+  (defadvice scala-eval-buffer (after pop-after-scala-eval-buffer activate)
+    (pop-to-buffer scala-inf-buffer-name)
+    (goto-char (point-max))))
