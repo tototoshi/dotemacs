4ca90433c143c484c76885a044a63d7df3769a93
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2011-05-20 00:56:11 +0900

    use from anything

--- my-moinmoin.el
+++ my-moinmoin.el
@@ -1,4 +1,5 @@
 (defvar my-moinmoin-url nil)
+(defvar my-moin-parse-command nil)
 
 (defun ewiki (title)
   (interactive "sTitle: ")
@@ -13,5 +14,21 @@
                                              my-moinmoin-url
                                              (http-url-encode title 'utf-8))))
         (t (message "Please specify my-moinmoin-url"))))
+
+(defun anything-my-moin-page-list ()
+  (split-string (shell-command-to-string
+                 (format "%s %s" my-moin-parse-command my-moinmoin-url)) "\n"))
+
+(setq anything-c-source-my-moin-page
+      '((name . "Page list")
+        (candidates . anything-my-moin-page-list)
+        (action
+         . (("View" . wiki)
+            ("Edit" . ewiki)
+            ))))
+
+(defun anything-my-moin ()
+  (interactive)
+  (anything '(anything-c-source-my-moin-page)))
 
 (provide 'my-moinmoin)
