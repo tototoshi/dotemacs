658f5c3aa9a163aa2db12d3cc4b0218bd6cb9611
toshiyuki <toshiyuki.takahashi@hde.co.jp>
2011-05-20 12:43:49 +0900

    add imenu-generic-expression

--- my-scala-config.el
+++ my-scala-config.el
@@ -1,5 +1,22 @@
-(when (require 'scala-mode nil t)
+(when (require 'scala-mode-auto nil t)
+  (require 'auto-highlight-symbol)
   (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
+  (add-hook 'scala-mode-hook
+            (lambda ()
+              (scala-electric-mode)
+              (setq imenu-generic-expression
+                    '(
+                      ("var" "\\(var +\\)\\([^(): ]+\\)" 2)
+                      ("val" "\\(val +\\)\\([^(): ]+\\)" 2)
+                      ("override def" "^[ \\t]*\\(override\\) +\\(def +\\)\\([^(): ]+\\)" 3)
+                      ("implicit def" "^[ \\t]*\\(implicit\\) +\\(def +\\)\\([^(): ]+\\)" 3)
+                      ("def" "^[ \\t]*\\(def +\\)\\([^(): ]+\\)" 2)
+                      ("trait" "\\(trait +\\)\\([^(): ]+\\)" 2)
+                      ("class" "^[ \\t]*\\(class +\\)\\([^(): ]+\\)" 2)
+                      ("case class" "^[ \\t]*\\(case class +\\)\\([^(): ]+\\)" 2)
+                      ("abstract class" "^[ \\t]*\\(abstract class +\\)\\([^(): ]+\\)" 2)
+                      ("object" "\\(object +\\)\\([^(): ]+\\)" 2)
+                      ))))
 
   ;; http://d.hatena.ne.jp/tototoshi/20110122/1295689870
   (defadvice scala-eval-region (after pop-after-scala-eval-region activate)
