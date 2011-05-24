017c4ffe913c3615d6a7a71e8db1917809b3b462
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2011-05-24 22:17:42 +0900

    dropdown-listにする

--- my-yasnippet-config.el
+++ my-yasnippet-config.el
@@ -3,7 +3,12 @@
 ;; Develop in ~/emacs.d/mysnippets, but also
 ;; try out snippets in ~/Downloads/interesting-snippets
 (setq yas/root-directory '("~/.emacs.d/dotemacs/yasnippet/snippets/"
-                           "~/.emacs.d/my-snippets/"))
+                           "~/.emacs.d/my-snippets/"
+                           "~/.emacs.d/AndreaCrotti-snippets/"))
 
 ;; Map `yas/load-directory' to every element
 (mapc 'yas/load-directory yas/root-directory)
+
+(when (require 'dropdown-list nil t)
+  (setq yas/prompt-functions '(yas/dropdown-prompt)))
+
