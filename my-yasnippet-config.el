c19ead7087471517e6864e896bf3b8dd8c4c85a4
toshiyuki <toshiyuki.takahashi@hde.co.jp>
2011-05-24 11:00:41 +0900

    change snippets directory

--- my-yasnippet-config.el
+++ my-yasnippet-config.el
@@ -3,7 +3,7 @@
 ;; Develop in ~/emacs.d/mysnippets, but also
 ;; try out snippets in ~/Downloads/interesting-snippets
 (setq yas/root-directory '("~/.emacs.d/dotemacs/yasnippet/snippets/"
-                           "~/.emacs.d/my-snippets/"))
+                           "~/.emacs.d/dotemacs/snippets/"))
 
 ;; Map `yas/load-directory' to every element
 (mapc 'yas/load-directory yas/root-directory)
