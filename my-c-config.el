52d425e06e77d66e7dab5c4af48d5882808b2aac
toshiyuki <toshiyuki.takahashi@hde.co.jp>
2011-06-08 14:09:03 +0900

    add some hooks

--- my-c-config.el
+++ my-c-config.el
@@ -1,3 +1,5 @@
-(add-hook 'c-mode-common-hook
-  (lambda()
-    (local-set-key  (kbd "C-c o") 'ff-find-other-file)))
+(add-hook 'c-mode-hook
+          (lambda()
+            (setq c-default-style "linux"
+                  c-basic-offset 4)
+            (local-set-key  (kbd "C-c o") 'ff-find-other-file)))
