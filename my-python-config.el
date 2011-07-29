1dd4d1d9c1801fa15c3614e4fb2401ee5b73af07
toshiyuki <toshiyuki.takahashi@hde.co.jp>
2011-07-29 13:00:26 +0900

    - electric-pair  - set py-create-index-function as imenu-create-index-function

--- my-python-config.el
+++ my-python-config.el
@@ -15,8 +15,20 @@
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
+(defun electric-pair ()
+  "Insert character pair without sournding spaces"
+  (interactive)
+  (let (parens-require-spaces)
+    (insert-pair)))
+
 (add-hook 'python-mode-hook
            (lambda ()
+             (define-key py-mode-map "\"" 'electric-pair)
+             (define-key py-mode-map "\'" 'electric-pair)
+             (define-key py-mode-map "(" 'electric-pair)
+             (define-key py-mode-map "[" 'electric-pair)
+             (define-key py-mode-map "{" 'electric-pair)
+             (setq imenu-create-index-function 'py-imenu-create-index)
              (set (make-variable-buffer-local 'beginning-of-defun-function)
                   'py-beginning-of-def-or-class)
              (setq outline-regexp "def\\|class ")))
