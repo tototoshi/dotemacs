956930508e7f2c2c7755b4d6019817c10e36621c
toshiyuki <toshiyuki.takahashi@hde.co.jp>
2011-08-02 20:18:24 +0900

    moinmoinmoinmoinmoinmoinmoinmoinmoinmoinmoinmoin

--- my-moinmoin.el
+++ my-moinmoin.el
@@ -3,6 +3,38 @@
 (defvar my-moinmoin-url nil)
 (defvar my-moin-parse-command nil)
 
+(defvar my-moinmoin-script "~/work/moin/moin.py")
+(defvar my-moinmoin-save-script "~/work/moin/moin-save.py")
+
+(defvar my-moinmoin-buffer-prefix "moinmoin-")
+
+(defun ewiki-with-emacs (title)
+  (interactive "sTitle: ")
+  (let ((buf (get-buffer-create
+              (format "%s%s" my-moinmoin-buffer-prefix title))))
+    (switch-to-buffer buf)
+    (erase-buffer)
+    (insert
+     (shell-command-to-string
+             (format "python %s %s" my-moinmoin-script title)))
+    (goto-char (point-min))
+    (replace-regexp "&lt;" "<")
+    (replace-regexp "&gt;" ">")
+    (goto-char (point-min))
+    (moinmoin-mode)))
+
+(defun my-moinmoin-save ()
+  (interactive)
+  (let ((page-name (string-replace-match my-moinmoin-buffer-prefix (buffer-name) ""))
+        (tmpfile "/tmp/my-moinmoin-tmpfile"))
+    (my-file-put-contents tmpfile (buffer-substring-no-properties (point-min) (point-max)))
+    (when page-name
+      (print
+       (shell-command-to-string
+       (format "python %s %s %s" my-moinmoin-save-script page-name tmpfile))))
+    (delete-file tmpfile)
+    (message (format "save %s" page-name))))
+
 (defun ewiki (title)
   (interactive "sTitle: ")
   (cond (my-moinmoin-url (browse-url (format "%s/%s?action=edit&editor=text"
@@ -27,6 +59,7 @@
         (action
          . (("View" . wiki)
             ("Edit" . ewiki)
+            ("Edit with emacs" . ewiki-with-emacs)
             ))))
 
 (defun anything-my-moin ()
