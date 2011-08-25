2ea3151acf7c3927fd1e19a11ccc9108550e60bd
toshiyuki <toshiyuki.takahashi@hde.co.jp>
2011-08-25 11:36:20 +0900

    find-file with C-m or clicking after 'ls'

--- my-shell-config.el
+++ my-shell-config.el
@@ -106,6 +106,52 @@
     (define-key 'eshell-ode-map ?\C-a 'eshell-bol)
     (define-key eshell-mode-map (kbd "C-c k") 'my-eshell-kill-line0)))
 
+;; lsしたあとにEnterでファイルを開ける
+;;; From: http://www.emacswiki.org/cgi-bin/wiki.pl/EshellEnhancedLS
+(eval-after-load "em-ls"
+  '(progn
+     ;; (defun ted-eshell-ls-find-file-at-point (point)
+     ;;          "RET on Eshell's `ls' output to open files."
+     ;;          (interactive "d")
+     ;;          (find-file (buffer-substring-no-properties
+     ;;                      (previous-single-property-change point 'help-echo)
+     ;;                      (next-single-property-change point 'help-echo))))
+     (defun pat-eshell-ls-find-file-at-mouse-click (event)
+       "Middle click on Eshell's `ls' output to open files.
+ From Patrick Anderson via the wiki."
+       (interactive "e")
+       (ted-eshell-ls-find-file-at-point (posn-point (event-end event))))
+     (defun ted-eshell-ls-find-file ()
+       (interactive)
+       (let ((fname (buffer-substring-no-properties
+                     (previous-single-property-change (point) 'help-echo)
+                     (next-single-property-change (point) 'help-echo))))
+         ;; Remove any leading whitespace, including newline that might
+         ;; be fetched by buffer-substring-no-properties
+         (setq fname (replace-regexp-in-string "^[ \t\n]*" "" fname))
+         ;; Same for trailing whitespace and newline
+         (setq fname (replace-regexp-in-string "[ \t\n]*$" "" fname))
+         (cond
+          ((equal "" fname)
+           (message "No file name found at point"))
+          (fname
+           (find-file fname)))))
+     (let ((map (make-sparse-keymap)))
+       ;;          (define-key map (kbd "RET")      'ted-eshell-ls-find-file-at-point)
+       ;;          (define-key map (kbd "<return>") 'ted-eshell-ls-find-file-at-point)
+       (define-key map (kbd "RET")      'ted-eshell-ls-find-file)
+       (define-key map (kbd "<return>") 'ted-eshell-ls-find-file)
+       (define-key map (kbd "<mouse-2>") 'pat-eshell-ls-find-file-at-mouse-click)
+       (defvar ted-eshell-ls-keymap map))
+     (defadvice eshell-ls-decorated-name (after ted-electrify-ls activate)
+       "Eshell's `ls' now lets you click or RET on file names to open them."
+       (add-text-properties 0 (length ad-return-value)
+                            (list 'help-echo "RET, mouse-2: visit this file"
+                                  'mouse-face 'highlight
+                                  'keymap ted-eshell-ls-keymap)
+                            ad-return-value)
+       ad-return-value)))
+
 ;;;;;;;;;;;;;;;
 ;; shell-pop ;;
 ;;;;;;;;;;;;;;;
