d11061ea41aa34bc328ffd624655f631c2c1472b
toshiyuki <t.toshi.0412@gmail.com>
2011-11-10 12:47:20 +0900

    Associate '.sbt' with scala-mode

--- my-scala-config.el
+++ my-scala-config.el
@@ -19,6 +19,7 @@
 
 
   (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
+  (add-to-list 'auto-mode-alist '("\\.sbt$" . scala-mode))
 
   (add-hook 'scala-mode-hook
             (lambda ()
