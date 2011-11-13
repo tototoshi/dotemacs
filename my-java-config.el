2bd328af319320c31724fe3ee09753f2c62d0fb3
toshiyuki <t.toshi.0412@gmail.com>
2011-11-13 11:40:27 +0900

    Fix the indentation of java-mode

--- my-java-config.el
+++ my-java-config.el
@@ -4,8 +4,14 @@
             (define-key java-mode-map "\'" 'electric-pair)
             (define-key java-mode-map "(" 'electric-pair)
             (define-key java-mode-map "{" 'electric-pair)
+
+            ;;                           
+            ;; http://stackoverflow.com/questions/7619399/emacs-fix-the-indentation-of-the-java-mode
+            (c-set-offset 'inexpr-class 0)
+
             (when (require 'java-mode-indent-annotations nil t)
               (java-mode-indent-annotations-setup))
+
             (setq c-default-style "linux"
                   c-basic-offset 4)
             (hs-minor-mode t)
