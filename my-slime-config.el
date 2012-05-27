6efc375feb4ddd44b647e34cfe1522d2f0a6cbd7
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2012-05-27 17:19:29 +0900

    Removed slime

--- my-slime-config.el
+++ my-slime-config.el
@@ -1,21 +1,21 @@
-(require 'slime)
+(when (require 'slime nil t)
 
-(setq slime-net-coding-system 'utf-8-unix)
+  (setq slime-net-coding-system 'utf-8-unix)
 
-(eval-after-load "slime"
-  '(progn
-     (message "Slime configuration...")
-     (slime-setup '(slime-fancy
-                    slime-repl
-                    slime-asdf
-                    ))))
+  (eval-after-load "slime"
+    '(progn
+       (message "Slime configuration...")
+       (slime-setup '(slime-fancy
+                      slime-repl
+                      slime-asdf
+                      ))))
 
+  (require 'ac-slime)
+  (add-hook 'slime-mode-hook 'set-up-slime-ac)
+  (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 
+  )
 
-(require 'ac-slime)
-(add-hook 'slime-mode-hook 'set-up-slime-ac)
-(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 
 (provide 'my-slime-config)
-
 
