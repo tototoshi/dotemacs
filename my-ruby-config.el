01395a942bc9977b3ffea858cda2844a7682f601
Toshiyuki Takahashi <t-takahashi@m3.com>
2012-06-17 22:59:33 +0900

    ruby-mode のインデントをいい感じにする

--- my-ruby-config.el
+++ my-ruby-config.el
@@ -2,6 +2,24 @@
            (require 'ruby-electric nil t))
   (require 'ruby-mode)
   (require 'ruby-electric)
+
+  (setq ruby-deep-indent-paren-style nil)
+
+  (defadvice ruby-indent-line (after unindent-closing-paren activate)
+    (let ((column (current-column))
+          indent offset)
+      (save-excursion
+        (back-to-indentation)
+        (let ((state (syntax-ppss)))
+          (setq offset (- column (current-column)))
+          (when (and (eq (char-after) ?\))
+                     (not (zerop (car state))))
+            (goto-char (cadr state))
+            (setq indent (current-indentation)))))
+      (when indent
+        (indent-line-to indent)
+        (when (> offset 0) (forward-char offset)))))
+
   (add-hook 'ruby-mode-hook
             (lambda ()
               (ruby-electric-mode))))
