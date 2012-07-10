3b853b6347a804c826b112b42e97017ce024bbff
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2012-07-10 22:02:15 +0900

    Add keybinding

--- my-markdown-mode-config.el
+++ my-markdown-mode-config.el
@@ -1,3 +1,5 @@
 (require 'markdown-mode)
 (add-to-list 'auto-mode-alist '("\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\.markdown$" . markdown-mode))
+(define-key markdown-mode-map (kbd "<tab>") 'yas/expand)
+
