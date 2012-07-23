ef8a127aa61b0784e29ee01e9b7e9cc480a9b099
Toshiyuki Takahashi <t-takahashi@m3.com>
2012-07-23 11:29:42 +0900

    Updated zencoding configuration

--- my-zencoding-mode-config.el
+++ my-zencoding-mode-config.el
@@ -3,5 +3,6 @@
 ;;;;;;;;;;;;;;;;;;;;
 (require 'zencoding-mode)
 (add-hook 'sgml-mode-hook 'zencoding-mode)
+(add-hook 'nxml-mode-hook 'zencoding-mode)
 (define-key zencoding-mode-keymap (kbd "C-c C-m") 'zencoding-expand-line)
 (define-key zencoding-preview-keymap (kbd "C-c C-m") 'zencoding-preview-accept)
