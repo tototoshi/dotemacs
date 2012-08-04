6ee71f606b355fcc66c4b4fe8e8a29fa40e3f5b0
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2012-08-04 23:06:40 +0900

    Added paredit

--- my-emacs-lisp-mode-config.el
+++ my-emacs-lisp-mode-config.el
@@ -1,6 +1,8 @@
 (require 'auto-complete)
 (add-hook 'emacs-lisp-mode-hook
           (lambda ()
+            (when (require 'paredit nil t)
+              (enable-paredit-mode))
             (modify-syntax-entry ?- "w")  ; now '-' is not considered a word-delimiter
             (setq ac-source '(ac-source-yasnippet))))
 
