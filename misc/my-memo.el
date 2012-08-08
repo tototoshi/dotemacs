fef999611ec7371d9a0e1d75850d6487ab7b0532
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2012-08-08 22:19:34 +0900

    Fixed my-memo.el

--- misc/my-memo.el
+++ misc/my-memo.el
@@ -4,13 +4,15 @@
 (defun memo (file-type)
   (interactive "sFILE_TYPE: ")
   (let* ((dir (concat (file-name-as-directory memo-dir)
-                     (format-time-string "%Y%m%d" (current-time))))
-         (file (concat (file-name-as-directory dir) "memo_%Y%m%d%H%M%S" "." file-type)))
+                      (format-time-string "%Y%m%d" (current-time))))
+         (file (concat (file-name-as-directory dir)
+                       (format-time-string "memo_%Y%m%d%H%M%S" (current-time))
+                       "." file-type)))
     (when (one-window-p)
       (split-window))
     (when (not (file-exists-p dir))
       (make-directory dir))
-    (find-file file))
+    (find-file file)))
 
 
 
