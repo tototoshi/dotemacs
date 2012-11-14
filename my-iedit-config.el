da58906e2b2a393e2653c27a868a20c8d449f6ac
toshiyuki <t.toshi.0412@gmail.com>
2012-11-14 23:29:57 +0900

    Changed key-binding of C-h on iedit-mode

--- my-iedit-config.el
+++ my-iedit-config.el
@@ -1,7 +1,5 @@
 ;; https://github.com/victorhge/iedit
 (when (require 'iedit nil t)
   ;; settings here
-  (define-key iedit-mode-map (kbd "C-h") 'delete-backward-char)
   )
-
 (provide 'my-iedit-config)
