8ac6bd04fc094bce8c82ab864acc6cbcf0fcd1cd
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2013-02-03 16:45:26 +0900

    Added ido.el Fixed a warning when haskell-mode is not found in load-path.

--- my-haskell-mode-config.el
+++ my-haskell-mode-config.el
@@ -1,18 +1,18 @@
-(require 'haskell-mode )
+(when (require 'haskell-mode nil t)
+  (add-to-list 'auto-mode-alist '("\.hs$" . haskell-mode))
+  (add-to-list 'auto-mode-alist '("\\.lhs$" . literate-haskell-mode))
 
-(add-to-list 'auto-mode-alist '("\.hs$" . haskell-mode))
-(add-to-list 'auto-mode-alist '("\\.lhs$" . literate-haskell-mode))
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+  ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+  ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+  (add-hook 'haskell-mode-hook
+            (progn
+              (define-key haskell-mode-map "\"" 'electric-pair)
+              (define-key haskell-mode-map "\'" 'electric-pair)
+              (define-key haskell-mode-map "(" 'electric-pair)
+              (define-key haskell-mode-map "[" 'electric-pair)
+              (define-key haskell-mode-map "{" 'electric-pair)
+              )))
 
-(add-hook 'haskell-mode-hook
-          (progn
-            (define-key haskell-mode-map "\"" 'electric-pair)
-            (define-key haskell-mode-map "\'" 'electric-pair)
-            (define-key haskell-mode-map "(" 'electric-pair)
-            (define-key haskell-mode-map "[" 'electric-pair)
-            (define-key haskell-mode-map "{" 'electric-pair)
-            ))
