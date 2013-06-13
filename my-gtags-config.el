0f52b60f05f52d3bbfb7eed6577557037f994bb2
tototoshi <t.toshi.0412@gmail.com>
2013-06-13 16:43:25 +0900

    Introduced helm-gtags

--- my-gtags-config.el
+++ my-gtags-config.el
@@ -1,11 +1,12 @@
 (autoload 'gtags-mode' "gtags" "" t)
+(autoload 'gtags-mode' "helm-gtags" "" t)
 (setq gtags-mode-hook
       '(lambda ()
-         (local-set-key "\M-t" 'gtags-find-tag)
-         (local-set-key "\M-r" 'gtags-find-rtag)
-         (local-set-key "\M-s" 'gtags-find-symbol)
-         (local-set-key "\M-o" 'gtags-find-file)
-         (local-set-key "\C-t" 'gtags-pop-stack)
+         (local-set-key "\M-t" 'helm-gtags-find-tag)
+         (local-set-key "\M-r" 'helm-gtags-find-rtag)
+         (local-set-key "\M-s" 'helm-gtags-find-symbol)
+         (local-set-key "\M-o" 'helm-gtags-find-file)
+         (local-set-key "\C-t" 'helm-gtags-pop-stack)
          ))
 
 (provide 'my-gtags-config)
