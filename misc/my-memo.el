23763eab81e3a37931237590666cbedb5405de65
tototoshi <t.toshi.0412@gmail.com>
2013-08-13 19:18:09 +0900

    Use ".txt" as default

--- misc/my-memo.el
+++ misc/my-memo.el
@@ -7,14 +7,11 @@
                       (format-time-string "%Y%m%d" (current-time))))
          (file (concat (file-name-as-directory dir)
                        (format-time-string "memo_%Y%m%d%H%M%S" (current-time))
-                       "." file-type)))
+                       "." (if (string= "" file-type) "txt" file-type))))
     (when (one-window-p)
       (split-window))
     (when (not (file-exists-p dir))
       (make-directory dir))
     (find-file file)))
-
-
-
 
 (provide 'my-memo)
