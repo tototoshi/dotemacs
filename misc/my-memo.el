1764b68ea84643e471f8c35b913db2df2e87f099
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2013-08-13 22:38:36 +0900

    Refactoring Update

--- misc/my-memo.el
+++ misc/my-memo.el
@@ -1,6 +1,10 @@
 ;; memo
 (defvar memo-dir "~/tmp/memo")
 
+(defun memo-ensure-dir-exists (file)
+  (unless (file-exists-p file)
+    (make-directory file t)))
+
 (defun memo (file-type)
   (interactive "sFILE_TYPE: ")
   (let* ((dir (concat (file-name-as-directory memo-dir)
@@ -8,10 +12,9 @@
          (file (concat (file-name-as-directory dir)
                        (format-time-string "memo_%Y%m%d%H%M%S" (current-time))
                        "." (if (string= "" file-type) "txt" file-type))))
+    (memo-ensure-dir-exists dir)
     (when (one-window-p)
       (split-window))
-    (when (not (file-exists-p dir))
-      (make-directory dir))
     (find-file file)))
 
 (provide 'my-memo)
