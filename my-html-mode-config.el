e7e3c9818d5189286ed89e71c20256b4adae9492
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2013-10-06 13:59:24 +0900

    Added .erb to auto-mode-alist

--- my-html-mode-config.el
+++ my-html-mode-config.el
@@ -1,3 +1,4 @@
+(add-to-list 'auto-mode-alist '("\\.erb$" . html-mode))
 (add-hook 'sgml-mode-hook (lambda ()
                    (define-key html-mode-map (kbd "C-c C-j") 'jaunte)
                    (setq sgml-basic-offset 4)))
