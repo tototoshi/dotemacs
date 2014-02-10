7f6b7f7e441d3fcc5d422086a508ed60f0b52fca
toshi <toshi@pixiv.com>
2014-02-10 11:36:32 +0900

    Updated my-java-config

--- my-java-config.el
+++ my-java-config.el
@@ -1,47 +1,8 @@
 (require 'java-mode-indent-annotations)
 
-(require 'flymake)
-(require 'flymake-mvn)
-
 (add-to-list 'auto-mode-alist '("\\.java$" . java-mode))
 (add-to-list 'auto-mode-alist '("\\.aj$" . java-mode))
 (add-to-list 'auto-mode-alist '("\\.tag$" . html-mode))
-
-(setq helm-c-source-my-java-imports-java-class-list
-      "~/.emacs.d/dotemacs/data/imports")
-
-(setq helm-c-source-my-java-imports-java-annotations-list
-      "~/.emacs.d/dotemacs/data/annotaions")
-
-(defvar helm-c-source-my-java-imports
-  '((name . "Java Imports")
-    (requires-pattern . 0)
-    (candidates-file helm-c-source-my-java-imports-java-class-list updating)
-    (action . (("insert". (lambda (x)
-                            (save-excursion
-                              (goto-line 3)
-                              (insert (format "import %s;\n" x))
-                              (my-java-sort-import-lines)
-                            )))))))
-
-(defvar helm-c-source-my-java-annotations
-  '((name . "Java annotations")
-    (requires-pattern . 0)
-    (candidates-file helm-c-source-my-java-imports-java-annotations-list updating)
-    (action . (("insert". (lambda (x) (insert x)))))))
-
-(defun helm-my-java-imports ()
-  (interactive)
-  (helm '(helm-c-source-my-java-imports)))
-
-(defun helm-my-java-annotations ()
-  (interactive)
-  (helm '(helm-c-source-my-java-annotations)))
-
-(defun my-java-semicolon ()
-  (interactive)
-  (end-of-line)
-  (insert ";"))
 
 (add-hook 'java-mode-hook
           (lambda ()
@@ -69,10 +30,6 @@
                   c-basic-offset 4)
             (hs-minor-mode t)
             (hideshowvis-enable)
-
-            (flymake-java-mvn-mode-hook)
-            (flymake-mode)
-
             ))
 
 (require 'compile)
@@ -112,90 +69,6 @@
 ;;  This assumes a Windows drive letter at the beginning
 (add-to-list 'compilation-error-regexp-alist '("^\\([a-zA-Z]:.*\\):\\[\\([0-9]+\\),\\([0-9]+\\)\\]" 1 2 3))
 
-
-(defun my-java-gen-javadoc ()
-  (interactive)
-  (let* ((comment-open "/**\n")
-         (comment-close "*/\n")
-         (signature (my-java-parse-method-signature (thing-at-point 'line)))
-         )
-    (insert (concat
-             comment-open
-             (funcall #'string-join
-                      (loop for i in (elt signature 3)
-                            collect (format "* @param %s\n" (second i))))
-             comment-close
-             ))))
-
-(defun my-java-parse-method-signature (line)
-  (let* ((words (mapcar #'string-trim (split-string line " ")))
-         (access (first words))
-         (type (second words))
-         (method-name-and-args (string-join (drop 2 words) " "))
-         (name (substring method-name-and-args
-                          0
-                          (string-index-of ?\( method-name-and-args)))
-         (args (mapcar #'(lambda (x) (split-string x " "))
-                       (mapcar #'string-trim
-                               (split-string (substring method-name-and-args
-                                                        (1+ (string-index-of ?\( method-name-and-args))
-                                                        (string-index-of ?\) method-name-and-args))
-                                             ",")))))
-    (list access type name args)
-    ))
-
-;;
-;; Utility for import
-;;
-(defun my-java-first-import-point ()
-  (save-excursion
-    (goto-char (point-min))
-    (when (re-search-forward "^import " nil t)
-      (forward-line 0)
-      (point))))
-
-(defun my-java-last-import-point ()
-  (save-excursion
-    (goto-char (point-max))
-    (when (re-search-backward "^import " nil t)
-      (point))))
-
-(defun my-java-package-point ()
-  (save-excursion
-    (goto-char (point-min))
-  (when (re-search-forward "^package " nil t)
-    (forward-line 0)
-    (point))))
-
-(defun my-java-find-import-point ()
-  (cond ((or (my-java-first-import-point) (my-java-last-import-point))
-         (save-excursion
-           (goto-char (my-java-last-import-point))
-           (forward-line 1)
-           (point)))
-        (t
-         (save-excursion
-           (goto-char (my-java-package-point))
-           (forward-line 2)
-           (point)))))
-
-(defun my-java-import (cls)
-  (interactive "sClass: ")
-  (save-excursion
-    (goto-char (my-java-find-import-point))
-    (insert (format "import %s;\n" cls))))
-
-(defun my-java-sort-import-lines ()
-  (interactive)
-  (when (and (my-java-first-import-point)
-             (my-java-last-import-point))
-    (let ((begin (my-java-first-import-point))
-          (end (save-excursion
-                 (goto-char (my-java-last-import-point))
-                 (forward-line 1)
-                 (point))))
-      (sort-lines nil begin end))))
-
 (defun my-java-ascii-to-native ()
   (interactive)
   (let ((p (point)))
@@ -224,11 +97,6 @@
                                     global-semanticdb-minor-mode
                                     global-semantic-idle-summary-mode
                                     global-semantic-mru-bookmark-mode))
-  (semantic-mode 1)
-  )
+  (semantic-mode 1))
 
 (provide 'my-java-config)
-
-
-
-
