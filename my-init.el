84ed2c08cd94b9bb9f9d97f5b18debc790176505
toshi <toshi@pixiv.com>
2014-02-14 10:17:14 +0900

    autoload doesn't work for php-mode

--- my-init.el
+++ my-init.el
@@ -13,11 +13,11 @@
 (require 'my-face)
 (require 'my-emacs-lisp-mode-config)
 (load "my-java-config.el")
+(load "my-php-mode-config.el")
 (autoload 'yas/minor-mode-on "my-yasnippet-config.el" nil t)
 (autoload 'delete-line "delete-line" nil t)
 (autoload 'sql-mode "my-sql-mode-config" nil t)
 (autoload 'ruby-mode "my-ruby-config" nil t)
-(autoload 'php-mode "my-php-mode-config.el" nil t)
 (autoload 'python-mode "my-python-config.el" nil t)
 (autoload 'gtags-mode' "gtags" nil t)
 (eval-after-load "gtags"
