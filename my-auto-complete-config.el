5c2863a64045aac78e56f5d7dc39e4e53fc79f00
toshi <toshi@pixiv.com>
2014-02-14 10:18:08 +0900

    Updated config for auto-complete

--- my-auto-complete-config.el
+++ my-auto-complete-config.el
@@ -5,11 +5,8 @@
 (require 'auto-complete)
 ;;(ac-config-default)
 (global-auto-complete-mode t)
-(add-to-list 'ac-modes 'org-mode)
 (add-to-list 'ac-modes 'python-mode)
-(add-to-list 'ac-modes 'text-mode)
 (add-to-list 'ac-modes 'shell-script-mode)
-(add-to-list 'ac-modes 'fundamental-mode)
 (add-to-list 'ac-modes 'lisp-interaction-mode)
 (add-to-list 'ac-modes 'perl-mode)
 (add-to-list 'ac-modes 'php-mode)
@@ -21,7 +18,7 @@
 (add-to-list 'ac-modes 'java-mode)
 
 ;; 大文字・小文字を区別しない
-(setq ac-ignore-case t)
+(setq ac-ignore-case nil)
 
 (setq ac-cursor-color "White")
 
