d8c5f7bafcf01723c54b5476b64f7c66b3c6d58a
toshi <toshi@pixiv.com>
2014-05-08 14:57:07 +0900

    Fixed 'x', 'X' command

--- my-view-mode-config.el
+++ my-view-mode-config.el
@@ -1,11 +1,6 @@
-;;;;;;;;;;;;;;;
-;; view-mode ;;
-;;;;;;;;;;;;;;;
 (require 'view)
-(global-set-key (kbd "M-i") 'view-mode)
 
-;(defadvice find-file (after read-only activate)
-;  (toggle-read-only t))
+(global-set-key (kbd "M-i") 'view-mode)
 
 (defvar my-view-mode-prefix "my-view-mode-")
 
@@ -18,18 +13,26 @@
   (intern (concat my-view-mode-prefix key-name)))
 
 (defmacro my-make-view-mode-in-command (key key-name fn)
-    `(progn (defun ,(my-view-mode-fn-name key-name) ()
-              (interactive)
-              (in-view-mode
-               (funcall ,fn)))
-            (define-key view-mode-map ,key (quote ,(my-view-mode-fn-name key-name)))))
+  `(progn (defun ,(my-view-mode-fn-name key-name) ()
+            (interactive)
+            (in-view-mode
+             (funcall ,fn)))
+          (define-key view-mode-map ,key (quote ,(my-view-mode-fn-name key-name)))))
 
 (defmacro my-make-view-mode-out-command (key key-name fn)
-    `(progn (defun ,(my-view-mode-fn-name key-name) ()
-              (interactive)
-              (view-mode)
-	      (funcall ,fn))
-            (define-key view-mode-map ,key (quote ,(my-view-mode-fn-name key-name)))))
+  `(progn (defun ,(my-view-mode-fn-name key-name) ()
+            (interactive)
+            (view-mode -1)
+            (funcall ,fn))
+          (define-key view-mode-map ,key (quote ,(my-view-mode-fn-name key-name)))))
+
+(defmacro my-make-view-mode-tmp-out-command (key key-name fn)
+  `(progn (defun ,(my-view-mode-fn-name key-name) ()
+            (interactive)
+            (view-mode -1)
+            (funcall ,fn)
+            (view-mode 1))
+          (define-key view-mode-map ,key (quote ,(my-view-mode-fn-name key-name)))))
 
 (defun my-view-mode-r (new-char)
   (interactive "sChar: ")
@@ -41,10 +44,10 @@
 (my-make-view-mode-out-command "a" "a" #'forward-char)
 (my-make-view-mode-out-command "A" "A" #'(lambda () (move-end-of-line 1)))
 (my-make-view-mode-out-command "\C-d" "delete-char" #'(lambda () (delete-char 1)))
-(my-make-view-mode-in-command "x" "x" #'(lambda () (delete-char 1)))
-(my-make-view-mode-in-command "X" "X" #'(lambda () (delete-backward-char 1)))
+(my-make-view-mode-tmp-out-command "x" "x" #'(lambda () (delete-char 1)))
+(my-make-view-mode-tmp-out-command "X" "X" #'(lambda () (delete-backward-char 1)))
+(my-make-view-mode-tmp-out-command "u" "u" #'undo)
 (my-make-view-mode-in-command " " "space" #'(lambda () nil))
-(my-make-view-mode-in-command "u" "u" #'undo)
 (my-make-view-mode-in-command "0" "0" #'(lambda () (move-beginning-of-line 1)))
 (my-make-view-mode-in-command "$" "$" #'(lambda () (move-end-of-line 1)))
 (my-make-view-mode-in-command [backspace] "bs" #'delete-backward-char)
@@ -63,4 +66,3 @@
                              (define-key view-mode-map "?" 'isearch-backward)
                              (define-key view-mode-map "/" 'isearch-forward)
                              (define-key view-mode-map "r" 'my-view-mode-r)))
-
