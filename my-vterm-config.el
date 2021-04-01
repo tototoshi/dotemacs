3509583bd38d4edcd6f4d33f9ba9ac1c90f91539
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2021-04-01 15:20:07 +0900

    Add a keybinding for vterm

--- my-vterm-config.el
+++ my-vterm-config.el
@@ -5,5 +5,7 @@
   (lambda () (interactive) (vterm-send-key (kbd "C-h"))))
 (define-key vterm-mode-map (kbd "C-c")
   (lambda () (interactive) (vterm-send-key (kbd "C-c"))))
+(define-key vterm-mode-map (kbd "C-u")
+  (lambda () (interactive) (vterm-send-key (kbd "C-u"))))
 
 (global-set-key (kbd "C-j") 'vterm)
