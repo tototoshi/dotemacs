0387b0eff30d3302bf957115ac5834f565901945
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2021-04-02 13:18:00 +0900

    Update alc.el

--- alc.el
+++ alc.el
@@ -1,25 +1,9 @@
-(require 'http-get)
-
-(defvar alc-toppage-url "http://eow.alc.co.jp")
-(defvar alc-encoding 'utf-8)
-(defvar alc-encoding-str "UTF-8")
-(defvar alc-http-param-alist '(("ref" . "sa")))
-
-(defun alc-http-param ()
-  (reduce #'(lambda (x y) (concat x "&" y))
-          (mapcar #'(lambda (cons)
-                      (format "%s=%s"
-                              (first cons)
-                              (http-url-encode (rest cons) alc-encoding)))
-                  alc-http-param-alist)))
+(defvar alc-toppage-url "https://eow.alc.co.jp")
+(defvar alc-search-page-url "https://eow.alc.co.jp/search")
 
 (defun alc-make-url (word)
   (cond ((string= word "") alc-toppage-url)
-        (t (format "%s/%s/%s/?%s"
-                   alc-toppage-url
-                   (http-url-encode word alc-encoding)
-                   alc-encoding-str
-                   (alc-http-param)))))
+        (t (concat alc-search-page-url "?q=" (url-hexify-string word)))))
 
 (defun alc (arg)
   (interactive "sWORD: ")
