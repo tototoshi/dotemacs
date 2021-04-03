2d257b3910c6737a535a1854e1aaaa9dec8d4493
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2021-04-03 11:10:43 +0900

    Add configurations for vterm

--- my-vterm-config.el
+++ my-vterm-config.el
@@ -1,11 +1,9 @@
-;; vterm
-(require 'vterm)
-
-(define-key vterm-mode-map (kbd "C-h")
-  (lambda () (interactive) (vterm-send-key (kbd "C-h"))))
-(define-key vterm-mode-map (kbd "C-c")
-  (lambda () (interactive) (vterm-send-key (kbd "C-c"))))
-(define-key vterm-mode-map (kbd "C-u")
-  (lambda () (interactive) (vterm-send-key (kbd "C-u"))))
-
-(global-set-key (kbd "C-j") 'vterm)
+(use-package vterm
+  :bind (("C-t" . vterm)
+         :map vterm-mode-map
+         ("C-c" . (lambda () (interactive) (vterm-send-key (kbd "C-c"))))
+         ("C-h" . (lambda () (interactive) (vterm-send-key (kbd "C-h"))))
+         ("C-t" . (lambda () (interactive) (switch-to-buffer (other-buffer))))
+         ("C-u" . (lambda () (interactive) (vterm-send-key (kbd "C-u"))))
+         :map dired-mode-map
+         ("C-t" . vterm)))
