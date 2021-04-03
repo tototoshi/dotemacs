4b0a3d57bf4cdd1061ae613385e470eff9c4e856
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2021-04-03 11:42:38 +0900

    Update dependencies

--- my-install-dependencies.el
+++ my-install-dependencies.el
@@ -1,44 +1,52 @@
 (defun my-install-dependencies-from-elpa ()
   ;; Install dependencies with elpa
   (require 'package)
-  (add-to-list 'package-archives
-               '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  (package-initialize)
-  (dolist (p '(ac-slime
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+  ;; (package-initialize)
+  (dolist (p '(
+               ac-slime
                apache-mode
                auto-highlight-symbol
-               auto-complete
                bm
                coffee-mode
+               diff-hl
                direx
-               dropdown-list
+               dracula-theme
                e2wm
-               gist
+               emojify
                go-mode
                haxe-mode
                helm
                helm-gtags
+               helm-ls-git
                http-post-simple
                iedit
-               jaunte
+               jade-mode
                js2-mode
                key-chord
+               macrostep
                magit
                markdown-mode
-               multi-term
+               neotree
                paredit
                php-mode
                popup
                popwin
+               protobuf-mode
                request
                rpm-spec-mode
                ruby-electric
-               scala-mode2
+               scala-mode
                sql-indent
+               tide
+               vterm
                w3m
                window-layout
+               yaml-mode
                yasnippet
-               zencoding-mode))
+               zencoding-mode
+               ))
     (unless (package-installed-p p)
       (package-refresh-contents) (package-install p))))
 
@@ -52,8 +60,7 @@
   (require 'el-get)
 
   (let ((el-get-sources
-         '((:name screen-lines :type github :pkgname "emacsmirror/screen-lines")
-           (:name helm-find-files-in-project :type github :pkgname "tototoshi/helm-find-files-in-project")
+         '((:name helm-find-files-in-project :type github :pkgname "tototoshi/helm-find-files-in-project")
            (:name memo :type github :pkgname "tototoshi/memo-el")
            (:name git-grep :type github :pkgname "tototoshi/git-grep-el")
            (:name python-mode :type github :pkgname "emacsmirror/python-mode")
