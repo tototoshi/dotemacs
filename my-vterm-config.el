c683253531b838284ac97184389b21d40526961d
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2021-04-03 16:24:06 +0900

    Put configurations for vterm into my-init.el

--- my-vterm-config.el
+++
@@ -1,9 +0,0 @@
-(use-package vterm
-  :bind (("C-t" . vterm)
-         :map vterm-mode-map
-         ("C-c" . (lambda () (interactive) (vterm-send-key (kbd "C-c"))))
-         ("C-h" . (lambda () (interactive) (vterm-send-key (kbd "C-h"))))
-         ("C-t" . (lambda () (interactive) (switch-to-buffer (other-buffer))))
-         ("C-u" . (lambda () (interactive) (vterm-send-key (kbd "C-u"))))
-         :map dired-mode-map
-         ("C-t" . vterm)))
