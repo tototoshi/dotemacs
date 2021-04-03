ce00fc8878a0d7dc49281db81c2d0ea261f7df49
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2021-04-03 15:39:42 +0900

    Update dependencies

--- my-install-dependencies.el
+++ my-install-dependencies.el
@@ -5,22 +5,17 @@
 
   ;; (package-initialize)
   (dolist (p '(
-               ac-slime
                apache-mode
                auto-highlight-symbol
                bm
                coffee-mode
                diff-hl
                direx
-               dracula-theme
-               e2wm
                emojify
                go-mode
-               haxe-mode
                helm
                helm-gtags
                helm-ls-git
-               http-post-simple
                iedit
                jade-mode
                js2-mode
@@ -31,10 +26,8 @@
                neotree
                paredit
                php-mode
-               popup
-               popwin
                protobuf-mode
-               request
+               python-mode
                rpm-spec-mode
                ruby-electric
                scala-mode
@@ -42,7 +35,6 @@
                tide
                vterm
                w3m
-               window-layout
                yaml-mode
                yasnippet
                zencoding-mode
@@ -63,10 +55,8 @@
          '((:name helm-find-files-in-project :type github :pkgname "tototoshi/helm-find-files-in-project")
            (:name memo :type github :pkgname "tototoshi/memo-el")
            (:name git-grep :type github :pkgname "tototoshi/git-grep-el")
-           (:name python-mode :type github :pkgname "emacsmirror/python-mode")
            (:name rfc-view :type github :pkgname "tototoshi/rfc-view-el")
            (:name tt-el :type github :pkgname "tototoshi/tt-el")
-           (:name auto-highlight-symbol :type github :pkgname "emacsmirror/auto-highlight-symbol")
            (:name hideshowvis :type github :pkgname "emacsmirror/hideshowvis")
            (:name java-mode-indent-annotations :type github :pkgname "emacsmirror/java-mode-indent-annotations"))))
     (dolist (p el-get-sources)
