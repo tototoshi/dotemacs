07116c47d859fd5e518c10642f5eb37e1a7ce84a
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2021-04-03 21:28:40 +0900

    Updated keybinds for vterm

--- my-init.el
+++ my-init.el
@@ -38,7 +38,6 @@
 (use-package vterm
   :bind (("C-t" . vterm)
          :map vterm-mode-map
-         ("C-c" . (lambda () (interactive) (vterm-send-key (kbd "C-c"))))
          ("C-h" . (lambda () (interactive) (vterm-send-key (kbd "C-h"))))
          ("C-t" . (lambda () (interactive) (switch-to-buffer (other-buffer))))
          ("C-u" . (lambda () (interactive) (vterm-send-key (kbd "C-u"))))
